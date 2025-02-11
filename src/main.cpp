b3dadefcf6453eeda3b9043623ce788e4681acea
elkks19 <rafafabiani1909@gmail.com>
2025-02-11 17:59:07 -0400

    terminado

--- src/main.cpp
+++ src/main.cpp
@@ -23,11 +23,43 @@
  ** aplicación de Scrum. 
  **/
 
+#include <cmath>
 #include <iostream>
+#include <list>
 
 using namespace std;
+bool isPrimo(int n, list<int>* primos) {
+	if (n == 1) return false;
+	if (n == 2) return true;
+	if (n % 2 == 0) return false;
+	for (int i = 3; i <= sqrt(n); i += 2) {
+		if (n % i == 0) return false;
+	}
+
+	return true;
+}
+
 int main (int argc, char *argv[]) {
-	cout << "Hello World!" << endl;
-	
+	int cantPrimos;
+	cout << "Ingrese la cantidad de números primos a mostrar: ";
+	cin >> cantPrimos;
+
+	list<int> primos;
+
+	for (int i = 1, j = 0; j < cantPrimos; i++) {
+		if (isPrimo(i)) {
+			primos.push_back(i);
+			j++;
+		}
+	}
+	cout << "[" << primos.front();
+
+	primos.pop_front();
+
+	for (int i : primos) {
+		cout << ", " << i;
+	}
+	cout << "]" << endl;
+
 	return 0;
 }
