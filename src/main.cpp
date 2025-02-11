0230a9ece3859486d8bedb55e8f3dfd2ff0512a0
elkks19 <rafafabiani1909@gmail.com>
2025-02-11 17:27:08 -0400

    primera commit

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+/**
+ ** Práctica de Diagnóstico - Serie de Números Primos con Scrum y Git
+ ** Objetivo:
+ ** Evaluar la capacidad de los estudiantes para resolver un problema sencillo de programación
+ ** aplicando Scrum y control de versiones con Git.
+ ** Instrucciones:
+ ** 1. Implementar la serie de números primos
+ ** ○ Utilizar cualquier lenguaje de programación.
+ ** ○ Aplicar cualquier método para generar los números primos.
+ ** 2. Subir la solución a GitHub
+ ** ○ Si tienen un repositorio en GitHub, subir el código y copiar el enlace y
+ ** captura de la ejecución del programa.
+ ** ○ Si no tienen un repositorio, crear un documento con:
+ ** ■ El código fuente de la solución.
+ ** ■ Una captura de pantalla con la ejecución del programa.
+ ** 3. Aplicar metodología Scrum
+ ** ○ Identificar qué lograron aplicar de Scrum.
+ ** Entrega:
+ ** ● Tiempo límite: ⏳ 30 minutos.
+ ** ● Subir el enlace del repositorio y la captura de pantalla o el documento con el
+ ** código y la captura de pantalla.
+ ** 🔹 Nota: La práctica se enfocará en la solución del problema, el uso de GitHub y la
+ ** aplicación de Scrum. 
+ **/
+
+#include <iostream>
+
+using namespace std;
+int main (int argc, char *argv[]) {
+	cout << "Hello World!" << endl;
+	
+	return 0;
+}
